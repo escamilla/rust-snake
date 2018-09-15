be3fda8254d4619bc899e4710fa91fbbbc43f5c7
= <joshua.escamilla@selu.edu>
2018-09-15 10:19:07 -0500

    Reduce repetition in methods for drawing snake

--- src/snake.rs
+++ src/snake.rs
@@ -1,6 +1,7 @@
 use std::collections::LinkedList;
 use std::f64::consts::PI;
 
+use piston_window::math::Matrix2d;
 use piston_window::types::Color;
 use piston_window::{rectangle, Context, G2d, Transformed};
 
@@ -8,7 +9,6 @@ use draw::{draw_block, Block, BLOCK_SIZE};
 
 const SNAKE_COLOR: Color = [0.34, 0.80, 0.17, 1.0];
 const EYE_COLOR: Color = [0.0, 0.0, 0.0, 1.0];
-const EYE_SIZE: f64 = (BLOCK_SIZE as f64) * 0.2;
 const TONGUE_COLOR: Color = [1.00, 0.50, 0.67, 1.0];
 
 #[derive(Copy, Clone, PartialEq)]
@@ -36,11 +36,6 @@ pub struct Snake {
     tail: Option<Block>,
 }
 
-struct Point {
-    x: f64,
-    y: f64,
-}
-
 impl Snake {
     pub fn new() -> Self {
         let mut body: LinkedList<Block> = LinkedList::new();
@@ -63,89 +58,53 @@ impl Snake {
         self.draw_tongue(context, g2d);
     }
 
-    fn draw_eyes(&self, context: &Context, g2d: &mut G2d) {
-        let (left_eye, right_eye) = self.get_eye_positions();
-        rectangle(
-            EYE_COLOR,
-            [left_eye.x, left_eye.y, EYE_SIZE, EYE_SIZE],
-            context.transform,
-            g2d,
-        );
-        rectangle(
-            EYE_COLOR,
-            [right_eye.x, right_eye.y, EYE_SIZE, EYE_SIZE],
-            context.transform,
-            g2d,
-        );
-    }
-
-    fn get_eye_positions(&self) -> (Point, Point) {
-        let (head_x, head_y): (u32, u32) = self.position();
-        let head_corner = Point {
-            x: (head_x * BLOCK_SIZE) as f64,
-            y: (head_y * BLOCK_SIZE) as f64,
-        };
-        let center_offset: f64 = (EYE_SIZE as f64) / 2.0;
-        let block_offset_third: f64 = (BLOCK_SIZE as f64) / 3.0;
-        let block_offset_two_thirds: f64 = block_offset_third * 2.0;
-        let (unadjusted_left_eye, unadjusted_right_eye) = match self.direction {
-            Direction::Up => (
-                Point {
-                    x: head_corner.x + block_offset_third,
-                    y: head_corner.y + block_offset_third,
-                },
-                Point {
-                    x: head_corner.x + block_offset_two_thirds,
-                    y: head_corner.y + block_offset_third,
-                },
-            ),
-            Direction::Down => (
-                Point {
-                    x: head_corner.x + block_offset_two_thirds,
-                    y: head_corner.y + block_offset_two_thirds,
-                },
-                Point {
-                    x: head_corner.x + block_offset_third,
-                    y: head_corner.y + block_offset_two_thirds,
-                },
-            ),
-            Direction::Left => (
-                Point {
-                    x: head_corner.x + block_offset_third,
-                    y: head_corner.y + block_offset_two_thirds,
-                },
-                Point {
-                    x: head_corner.x + block_offset_third,
-                    y: head_corner.y + block_offset_third,
-                },
-            ),
-            Direction::Right => (
-                Point {
-                    x: head_corner.x + block_offset_two_thirds,
-                    y: head_corner.y + block_offset_third,
-                },
-                Point {
-                    x: head_corner.x + block_offset_two_thirds,
-                    y: head_corner.y + block_offset_two_thirds,
-                },
-            ),
-        };
-        let adjusted_left_eye = Point {
-            x: unadjusted_left_eye.x - center_offset,
-            y: unadjusted_left_eye.y - center_offset,
-        };
-        let adjusted_right_eye = Point {
-            x: unadjusted_right_eye.x - center_offset,
-            y: unadjusted_right_eye.y - center_offset,
-        };
-        (adjusted_left_eye, adjusted_right_eye)
-    }
-
-    fn draw_tongue(&self, context: &Context, g2d: &mut G2d) {
+    fn transform_based_on_direction(&self, context: &Context) -> Matrix2d {
         let (head_x, head_y): (u32, u32) = self.position();
         let center_x = ((head_x * BLOCK_SIZE) as f64) + ((BLOCK_SIZE as f64) / 2.0);
         let center_y = ((head_y * BLOCK_SIZE) as f64) + ((BLOCK_SIZE as f64) / 2.0);
+        context
+            .transform
+            .trans(center_x, center_y)
+            .rot_rad(match self.direction {
+                Direction::Up => 0.0,
+                Direction::Right => PI * 0.5,
+                Direction::Down => PI,
+                Direction::Left => PI * 1.5,
+            })
+    }
+
+    fn draw_eyes(&self, context: &Context, g2d: &mut G2d) {
         let pixel_size = (BLOCK_SIZE as f64) * 0.1;
+        let transform = self.transform_based_on_direction(context);
+
+        rectangle(
+            EYE_COLOR,
+            [
+                pixel_size * -2.75,
+                pixel_size * -2.5,
+                pixel_size * 2.0,
+                pixel_size * 2.0,
+            ],
+            transform,
+            g2d,
+        );
+
+        rectangle(
+            EYE_COLOR,
+            [
+                pixel_size * 0.75,
+                pixel_size * -2.5,
+                pixel_size * 2.0,
+                pixel_size * 2.0,
+            ],
+            transform,
+            g2d,
+        );
+    }
+
+    fn draw_tongue(&self, context: &Context, g2d: &mut G2d) {
+        let pixel_size = (BLOCK_SIZE as f64) * 0.1;
+        let transform = self.transform_based_on_direction(context);
 
         rectangle(
             TONGUE_COLOR,
@@ -155,14 +114,7 @@ impl Snake {
                 pixel_size,
                 (pixel_size * 2.0),
             ],
-            context.transform
-                .trans(center_x, center_y)
-                .rot_rad(match self.direction {
-                    Direction::Up => 0.0,
-                    Direction::Right => PI * 0.5,
-                    Direction::Down => PI,
-                    Direction::Left => PI * 1.5,
-                }),
+            transform,
             g2d,
         );
 
@@ -174,15 +126,7 @@ impl Snake {
                 pixel_size,
                 pixel_size,
             ],
-            context
-                .transform
-                .trans(center_x, center_y)
-                .rot_rad(match self.direction {
-                    Direction::Up => 0.0,
-                    Direction::Right => PI * 0.5,
-                    Direction::Down => PI,
-                    Direction::Left => PI * 1.5,
-                }),
+            transform,
             g2d,
         );
 
@@ -194,15 +138,7 @@ impl Snake {
                 pixel_size,
                 pixel_size,
             ],
-            context
-                .transform
-                .trans(center_x, center_y)
-                .rot_rad(match self.direction {
-                    Direction::Up => 0.0,
-                    Direction::Right => PI * 0.5,
-                    Direction::Down => PI,
-                    Direction::Left => PI * 1.5,
-                }),
+            transform,
             g2d,
         );
     }
